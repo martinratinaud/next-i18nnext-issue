a5737a02fb39ee91018793058f013c2ec2382e20
Martin Ratinaud <martinratinaud@gmail.com>
2021-05-12 15:12:13 +0400

    feat: replace page in pages by page in module

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,50 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+// import Link from 'next/link'
+// import { useRouter } from 'next/router'
 
-import { useTranslation } from 'next-i18next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+// import { useTranslation } from 'next-i18next'
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
+// import { Header } from '../components/Header'
+// import { Footer } from '../components/Footer'
 
-const Homepage = () => {
+// const Homepage = () => {
 
-  const router = useRouter()
-  const { t } = useTranslation('common')
+//   const router = useRouter()
+//   const { t } = useTranslation('common')
 
-  return (
-    <>
-      <main>
-        <Header heading={t('h1')} title={t('title')} />
-        <div>
-          <Link
-            href='/'
-            locale={router.locale === 'en' ? 'de' : 'en'}
-          >
-            <button>
-              {t('change-locale')}
-            </button>
-          </Link>
-          <Link href='/second-page'>
-            <button
-              type='button'
-            >
-              {t('to-second-page')}
-            </button>
-          </Link>
-        </div>
-      </main>
-      <Footer />
-    </>
-  )
-}
+//   return (
+//     <>
+//       <main>
+//         <Header heading={t('h1')} title={t('title')} />
+//         <div>
+//           <Link
+//             href='/'
+//             locale={router.locale === 'en' ? 'de' : 'en'}
+//           >
+//             <button>
+//               {t('change-locale')}
+//             </button>
+//           </Link>
+//           <Link href='/second-page'>
+//             <button
+//               type='button'
+//             >
+//               {t('to-second-page')}
+//             </button>
+//           </Link>
+//         </div>
+//       </main>
+//       <Footer />
+//     </>
+//   )
+// }
 
-export const getStaticProps = async ({ locale }) => ({
-  props: {
-    ...await serverSideTranslations(locale, ['common', 'footer']),
-  },
-})
+// export const getStaticProps = async ({ locale }) => ({
+//   props: {
+//     ...await serverSideTranslations(locale, ['common', 'footer']),
+//   },
+// })
 
-export default Homepage
+// export default Homepage
+
+export { default, getStaticProps } from "../modules/Common/pages/home";
